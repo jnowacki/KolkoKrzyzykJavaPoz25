0c54dc2b57a81147fb4edf39b8924b445da59b37
Jedrzej Nowacki <contact@jnowacki.pl>
2020-03-28 19:31:35 +0100

    Zadania z todo

--- src/pl/jnowacki/Main.java
+++ src/pl/jnowacki/Main.java
@@ -22,13 +22,7 @@ public class Main {
         int boardSize = 3;
         int round = 1;
 
-        String[][] board = new String[boardSize][boardSize];
-
-        for (int i = 0; i < boardSize; i++){
-            for (int j = 0; j < boardSize; j++){
-                board[i][j] = " ";
-            }
-        }
+        String[][] board = initBoard(boardSize);
 
         do {
             System.out.println("Runda nr " + round);
@@ -37,49 +31,72 @@ public class Main {
             System.out.println("Gdzie chcesz postawic " + (round % 2 == 1 ? "o" : "x"));
 
             System.out.println("Podaj wiersz do postawienia tokena: ");
-            int row = Integer.valueOf(in.nextLine()) - 1;
+            int row = Integer.parseInt(in.nextLine()) - 1;
 
             System.out.println("Podaj kolumne do postawienia tokena: ");
-            int col = Integer.valueOf(in.nextLine()) - 1;
+            int col = Integer.parseInt(in.nextLine()) - 1;
 
             if(row < 0 || row > 2 || col < 0 || col > 2) {
+//                TODO: dodac sprawdzenie, czy pole jest puste
                 System.out.println("Niepoprawne dane");
                 continue;
             }
 
             board[row][col] = round % 2 == 1 ? "o" : "x";
 
-//            wyswietlanie
-            for (int i = 0; i < boardSize; i++){
-                for (int j = 0; j < boardSize; j++){
+            drawBoard(board, boardSize);
 
-//                    wyswietlanie pola
-                    System.out.print(board[i][j]);
-
-//                    rodziela pola w wierszu
-                    if(j < boardSize - 1) {
-                        System.out.print("|");
-                    }
-                }
-
-//                linia konca wiersza
-                if(i < boardSize - 1) {
-                    System.out.println("\n-----");
-                }
-
-////                dynamiczna wersja rodzialu wierszy:
-//                System.out.println();
-//                if(i < boardSize - 1) {
-//                    for (int k = 0; k < 2 * boardSize - 1; k++){
-//                        System.out.print("-");
-//                    }
-//                }
-//                System.out.println();
-            }
-
-            System.out.println("\n");
+//            TODO: warunek zwyciestwa i przypisanie zwyciezcy do zmiennej "winner"
 
             round += 1;
-        } while (winner.isEmpty() && round <= 9);
+        } while (shouldContinue(winner, round));
+
+        displaySummary(winner);
+    }
+
+    private static boolean shouldContinue(String winner, int round) {
+        return winner.isEmpty() && round <= 9;
+    }
+
+    private static void displaySummary(String winner) {
+        if(winner.isEmpty()) {
+            System.out.println("Byl remis");
+        } else {
+            System.out.println("Wygral gracz " + winner + ", gratulacje!");
+        }
+    }
+
+    private static void drawBoard(String[][] board, int length) {
+        for (int i = 0; i < length; i++){
+            for (int j = 0; j < length; j++){
+
+//                    wyswietlanie pola
+                System.out.print(board[i][j]);
+
+//                    rodziela pola w wierszu
+                if(j < length - 1) {
+                    System.out.print("|");
+                }
+            }
+
+//                linia konca wiersza
+            if(i < length - 1) {
+                System.out.println("\n-----");
+            }
+        }
+
+        System.out.println("\n");
+    }
+
+    private static String[][] initBoard(int boardSize){
+        String[][] board = new String[boardSize][boardSize];
+
+        for (int i = 0; i < boardSize; i++){
+            for (int j = 0; j < boardSize; j++){
+                board[i][j] = " ";
+            }
+        }
+
+        return board;
     }
 }
